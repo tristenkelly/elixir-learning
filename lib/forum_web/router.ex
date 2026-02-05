e1d4f77365b23fd6580d7261eb6766d085be8805
tristenkelly <tristenkelly98@gmail.com>
2026-02-04 20:24:05 -0500

    posts/users

--- lib/forum_web/router.ex
+++ lib/forum_web/router.ex
@@ -20,6 +20,12 @@ defmodule ForumWeb.Router do
     get "/", PageController, :home
   end
 
+  scope "/api", ForumWeb do
+    pipe_through :api
+    resources "/posts", PostController, except: [:new, :edit]
+    resources "/users", UserController, except: [:new, :edit]
+  end
+
   # Other scopes may use custom stacks.
   # scope "/api", ForumWeb do
   #   pipe_through :api
